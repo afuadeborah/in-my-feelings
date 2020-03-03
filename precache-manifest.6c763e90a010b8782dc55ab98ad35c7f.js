2bdb1c4953cf6a8c58c53faf60715c5a865dc1ab
Afua <afrimpong.15@gmail.com>
2020-03-03 16:59:48 -0500

    Updates

---
+++ precache-manifest.6c763e90a010b8782dc55ab98ad35c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30b2cf13a70b099a1bfc151dcdefc35",
+    "url": "/afuadeborah/afua-frimpong-project-5/index.html"
+  },
+  {
+    "revision": "ba23c85b81312ee53979",
+    "url": "/afuadeborah/afua-frimpong-project-5/static/css/main.dbb60db0.chunk.css"
+  },
+  {
+    "revision": "781bb38d3e8268c8e0b1",
+    "url": "/afuadeborah/afua-frimpong-project-5/static/js/2.bbbec730.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/afuadeborah/afua-frimpong-project-5/static/js/2.bbbec730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba23c85b81312ee53979",
+    "url": "/afuadeborah/afua-frimpong-project-5/static/js/main.fc90fcdc.chunk.js"
+  },
+  {
+    "revision": "bb80c91a7f1c061eaaba",
+    "url": "/afuadeborah/afua-frimpong-project-5/static/js/runtime-main.0e876b80.js"
+  }
+]);
