727a1d3f8a20f51857af5ea14092580a4212f80c
Afua <afrimpong.15@gmail.com>
2020-03-04 10:06:51 -0500

    API fix, do not mark I guess

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     const apiKey = 'a0b4a2a68217ad3b52fe53e7b7ba0679';
 
     axios({
-        url: 'http://ws.audioscrobbler.com/2.0/',
+        url: 'https://ws.audioscrobbler.com/2.0/',
         method: 'GET',
         responseType: 'json',
         params: {
